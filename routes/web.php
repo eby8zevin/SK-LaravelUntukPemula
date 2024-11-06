700010c793b599df819118340d3376517c718867
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-11-06 07:32:14 +0700

    Menambahkan Route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
